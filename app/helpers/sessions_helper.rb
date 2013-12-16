0031667831a20ced321b23687e461e94c99fe2c1
Hendrik Liebau <hendrik.liebau@sinnerschrader.com>
2013-12-16 20:56:04 +0100

    fixed a bug in current_user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,10 +17,8 @@ module SessionsHelper
   end
 
   def current_user
-    unless @current_user
-      remember_token = User.encrypt(cookies[:remember_token])
-      @current_user = User.find_by(remember_token: remember_token)
-    end
+    remember_token = User.encrypt(cookies[:remember_token])
+    @current_user ||= User.find_by(remember_token: remember_token)
   end
 
   def signed_in?
