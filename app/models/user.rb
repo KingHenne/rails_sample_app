1bb78e5c307eb2c5b8748a749adc6792ce5f364a
Hendrik Liebau <hendrik.liebau@sinnerschrader.com>
2013-12-16 13:35:34 +0100

    implemented secure passwords for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
+
+  has_secure_password
 
   before_save { self.email = email.downcase }
 
